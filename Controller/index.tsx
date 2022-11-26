d739cd38b55e753cbbbb26baf21a54669c3ccd7b
codedinsound <107017540+codedinsound@users.noreply.github.com>
2022-11-26 02:52:35 -0700

    1 file modified

--- Controller/index.tsx
+++ Controller/index.tsx
@@ -12,23 +12,40 @@ class LocalStorageWorker {
   }
 }
 
-class SessionManagerWorker {
-  static login(payload): void {
-    console.log(payload);
+async function getClientIPAddress() {
+  const res = await fetch('https://geolocation-db.com/json/');
+  const data = await res.json();
 
-    const encrypted = AES.encrypt(
-      payload.username,
-      payload.password
+  return data.IPv4;
+}
+
+class SessionManagerWorker {
+  static async login(data) {
+    console.log(data);
+
+    let IPv4: string = await getClientIPAddress();
+
+    const encryptedPassword = AES.encrypt(
+      data.username, // Message
+      data.password // Key
     ).toString();
 
-    console.log(encrypted);
+    console.log(encryptedPassword);
+
+    const payload = {
+      IPv4,
+      username: data.username,
+      password: encryptedPassword,
+    };
+
+    console.log(payload);
 
     fetch('https://gp-broomfield-neo-server.codedsound.repl.co/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ data: '' }),
+      body: JSON.stringify(payload),
     });
 
     // const bytes = AES.decrypt(cipher, payload.password);
