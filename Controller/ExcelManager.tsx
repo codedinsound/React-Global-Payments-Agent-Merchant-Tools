71e34ac1c9ed01daea416bb01e7ccd3bba6a0ef8
codedinsound <107017540+codedinsound@users.noreply.github.com>
2022-12-21 13:28:06 -0700

    1 file modified

--- Controller/ExcelManager.tsx
+++ Controller/ExcelManager.tsx
@@ -7,9 +7,21 @@ class ExcelManager {
 
   // MARK: Create an Excel Sheet Report form Historical Data
   static createNewExcelSheetReport(data): void {
-    var workbook = XLSX.utils.book_new();
+    // var workbook = XLSX.utils.book_new();
 
     const structure = [['', '', '', '', '']];
+
+    // var worksheet = XLSX.utils.aoa_to_sheet([
+    //   ["A1", "B1", "C1"],
+    //   ["A2", "B2", "C2"],
+    //   ["A3", "B3", "C3"]
+    // ]);
+
+    // XLSX.utils.book_append_sheet(workbook, worksheet, sheet_name);
+
+    // XLSX.writeFileXLSX(workbook, filename, opts);
+
+    console.log(structure);
   }
 }
 
